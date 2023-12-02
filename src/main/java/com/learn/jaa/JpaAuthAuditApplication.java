ff3afa6a0518860b01ee0540e5ce3168d451615a
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-12-02 11:57:56 -0700

    basic structure of spring boot application is added

---
+++ src/main/java/com/learn/jaa/JpaAuthAuditApplication.java
@@ -0,0 +1,13 @@
+package com.learn.jaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAuthAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAuthAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/jaa/JpaAuthAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.jaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAuthAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
