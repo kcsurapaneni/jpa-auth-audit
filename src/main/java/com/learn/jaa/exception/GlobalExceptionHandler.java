b29b20c88da4220d04da4a0e090cdc54b2176d73
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-12-11 11:21:54 -0700

    global exception handler is added

---
+++ src/main/java/com/learn/jaa/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.learn.jaa.exception;
+
+import lombok.extern.slf4j.*;
+import org.springframework.http.*;
+import org.springframework.security.access.*;
+import org.springframework.security.authentication.*;
+import org.springframework.web.*;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Krishna Chaitanya
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ProblemDetail> handleAccessDeniedException(AccessDeniedException e) {
+        log.error("Came to AccessDeniedException in GlobalExceptionHandler");
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
+        problemDetail.setTitle("Requested Resource is FORBIDDEN");
+        return new ResponseEntity<>(problemDetail, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetail> handleException(Exception e) {
+        log.error("Came to Exception in GlobalExceptionHandler");
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        problemDetail.setTitle("Server Side Error");
+        return new ResponseEntity<>(problemDetail, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException e) {
+        log.error("Came to BadCredentialsException in GlobalExceptionHandler");
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+}
