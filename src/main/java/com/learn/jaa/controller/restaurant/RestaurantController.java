bd9dd7fa61e2aea0251945953d337670244fdca1
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-12-13 13:03:52 -0700

    authorization is added at method level using @PreAuthorize

--- src/main/java/com/learn/jaa/controller/restaurant/RestaurantController.java
+++ src/main/java/com/learn/jaa/controller/restaurant/RestaurantController.java
@@ -1,5 +1,6 @@
 package com.learn.jaa.controller.restaurant;
 
+import org.springframework.security.access.prepost.*;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class RestaurantController {
 
     @GetMapping
+    @PreAuthorize("hasRole('admin')")
     public String hello() {
         return "Hello, restaurant!";
     }
